f7f91967a17d3cb410810874f7c0b254c267f38f
Daniel delas Cinco Ordilias <daniconix98@gmail.com>
2026-06-25 09:07:26 -0600

    FrontEndDocumentos

---
+++ FORECAST/Models/DocumentosViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace MotoRepuestosRojas.Models
+{
+    public class DocumentosViewModel
+    {
+        public int id { get; set; }
+        public int idCliente { get; set; }
+        public int idCondPago { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comentarios { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal TotalImpuestos { get; set; }
+        public decimal TotalDescuentos { get; set; }
+        public decimal TotalCompra { get; set; }
+        public string Moneda { get; set; }
+        public decimal PorDescto { get; set; }
+        public DetDocumentoViewModel[] Detalle { get; set; }
+        public int BaseEntry { get; set; }
+    }
+}
